327bce99dacdb8fd6a9d342ee0c1264cfe947f57
Urchmaney <kingsobino@gmail.com>
2019-08-28 21:31:02 +0000

    resolve route error

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,4 @@ Rails.application.routes.draw do
   get  '/posts/new', to: 'posts#new'
   post '/posts', to: 'posts#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end`
+end
